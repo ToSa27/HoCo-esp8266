a63894c1798d8516e8a829b7245265aa6a23aae7
Tobias Hoff <tobias@die-hoffs.net>
2016-08-24 20:20:21 +0200

    Fix project settings to use proper toolchain and add esptool2 as submodule

---
+++ common/include/hardware.h
@@ -0,0 +1,15 @@
+#ifndef __HOCO_HARDWARE_H__
+#define __HOCO_HARDWARE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define RESET_GPIO_NUM		16
+#define LED_GPIO_NUM		2
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
